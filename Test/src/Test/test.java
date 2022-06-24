1e1ad0f95b76540a0fd871633e16e5c9bc4ae95a
Caobaining <2113882192@qq.com>
2022-06-24 09:24:04 +0800

    第二次提交

---
+++ Test/src/Test/test.java
@@ -0,0 +1,10 @@
+package Test;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello wor1ld\n");
+		System.out.print("hello wo1rld\n");
+	}
+
+}
